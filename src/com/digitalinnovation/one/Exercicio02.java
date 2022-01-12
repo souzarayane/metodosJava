a05a3e08d08ce364ef8ad1d53a9a3998bccc8f49
Rayane Souza <73654615+souzarayane@users.noreply.github.com>
2022-01-12 14:32:40 -0300

    Inclusão do exercício02 - sobrecarga

---
+++ src/com/digitalinnovation/one/Exercicio02.java
@@ -0,0 +1,6 @@
+/*
+ * Crie uma aplicação que calcule a área dos 3 quadriláteros
+ * notáveis: quadrado, retângulo e trapézio.
+ * 
+ * Obs: use a sobrecarga
+ */
---
+++ src/com/digitalinnovation/one/Main2.java
@@ -0,0 +1,15 @@
+package com.digitalinnovation.one;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		//Quadrilátero
+		
+		System.out.println("Exercício quadrilátero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5, 5);
+		Quadrilatero.area(7, 8, 9);
+	}
+	
+}
