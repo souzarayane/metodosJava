61c619e2a7ef3ca1b44f0778d66927004c886506
Rayane Souza <73654615+souzarayane@users.noreply.github.com>
2022-01-12 15:03:58 -0300

    Inserir exercício3 mais slides

---
+++ src/com/digitalinnovation/one/Exercicio03.java
@@ -0,0 +1,4 @@
+/*
+*Recrie a aplicação que calcula a área dos 3 quadriláteros
+*notáveis. Agora faça os métodos retornarem valores.
+*/
---
+++ src/com/digitalinnovation/one/Main3.java
@@ -0,0 +1,16 @@
+package com.digitalinnovation.one;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		
+	//Quadrilátero
+		
+		System.out.println("Exercício quadrilátero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5, 5);
+		Quadrilatero.area(7, 8, 9);
+
+	}
+
+}
